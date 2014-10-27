16489e2e7e5743c82c351eed8128403d35fd774e
a123xxsp <a123xxsp@yahoo.gr>
2014-10-27 09:50:58 +0200

    old index moved to _old_half, new working to index.php
    code by George, reviewed and studied

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-07 16:36
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'openeclass';
+$mysqlPassword = 'openeclass';
+$mysqlMainDb = 'eclass';
+
+$hackerEarthKey = 'ef34f0279e1af1fd967cbf7bd6562bece9e05041';
+
